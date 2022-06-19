3c1c7f4384e3cc4af6e32a3de3d2bfd0745bde8a
Emre Dogan <emredogan@outlook.com>
2022-06-19 13:44:40 +0300

    Initial Commit

---
+++ OnTime/OnTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTimeApp.swift
+//  OnTime
+//
+//  Created by Emre Dogan on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct OnTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
